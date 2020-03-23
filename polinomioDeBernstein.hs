e948a86471540730b35125c2263dbf811b748dda
Blanca <blancacano2.71828@gmail.com>
2020-03-23 10:23:59 +0100

    [polinomioDeBernestein] Añado cabecera de tipos, borro función y gitignore

--- polinomioDeBernstein.hs
+++ polinomioDeBernstein.hs
@@ -1,11 +1,13 @@
 {-
 	Polinomio de Bernstein
 
+Utilizado en el método constructivo de la aproximación de Weierstrass,
+que dice que toda función continua en un intervalo cerrado se puede aproximar
+todo lo que se desee a un polinomio ( dicho polinomio es el de Bernstein). 
+
 Fuentes:
 https://es.wikipedia.org/wiki/Polinomio_de_Bernstein
 https://es.wikipedia.org/wiki/Teorema_de_aproximaci%C3%B3n_de_Weierstrass
-
-Utilizado en el método constructivo de la aproximación de Weierstrass, que dice que toda función continua en un intervalo cerrado se puede aproximar todo lo que se desee a un polinomio ( dicho polinomio es el de Bernstein.
 
 Granada 23/III/20 
 -}
@@ -22,7 +24,7 @@ c n k = factorial n / ( (factorial k) * factorial (n-k))
 Notación n iterador, f  función continua en el intervalo [a,b] y x valor a evaluar
 -}
 
-g x = 1.1*x
+bernstein :: (Enum a, Floating a) => a -> (a -> a) -> a -> a -> a -> a
 bernstein n f x a b =  sum [f(k*(b-a)/n + a)*( c n k )* (x-a)**k * (b -x)**(n-k)/ (b-a)**n| k<-[0..n] ]
 
 
